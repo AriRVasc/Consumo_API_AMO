ca0429464d9ec7043ce1629cb77dc8a57a9e1af5
Ariadne Rodrigues Vasconcelos <ariadnerv17@gmail.com>
2024-01-04 08:59:00 -0300

    Ajustes na interface

--- app.py
+++ app.py
@@ -2,43 +2,54 @@ from flask import Flask, render_template
 import pandas as pd
 import plotly.express as px
 import requests
+import pickle
 
 app = Flask(__name__)
 
 
-@app.route('/') # Rota principal para renderizar a dashboard
+@app.route('/') 
 def index():
     # obter dados da API
-    api_url = 'https://brisa-ts.dev.amo.delivery/api/esp32amo?start_at=2023-11-13'
+    api_url = 'https://brisa-ts.dev.amo.delivery/api/esp32amo?start_at=2023-11-24?end_at'
     response = requests.get(api_url)
     data = response.json()
 
-    # transformar dados em DataFrame usando Pandas
-    df = pd.DataFrame(data['data'])  # Use 'data' para obter a lista de dados
+    
+    df = pd.DataFrame(data['data'])  
 
-    # converta a coluna 'created_at' para o tipo de dados de data/hora
+   
     df['created_at'] = pd.to_datetime(df['created_at'])
 
-    # formatar 'created_at' para exibir apenas dia, mês, e hora
+   
     df['Tempo'] = df['created_at'].dt.strftime('%d-%m %H:%M:%S')
 
-    # Calcular a última medida de temperatura interna e externa
+    
+    with open('modelo_knn.pkl', 'rb') as file:
+        modelo_carregado = pickle.load(file)
+
+   
     ultima_temperatura_int = df['temperature_int'].iloc[-1]
     ultima_temperatura_ext = df['temperature_ext'].iloc[-1]
     ultima_umidade_int = df['humidity_int'].iloc[-1]
     ultima_umidade_ext = df['humidity_ext'].iloc[-1]
+    ultima_temperatura_alimento = df['temperature_alimento'].iloc[-1]
 
-    # Criar gráfico usando Plotly (Temperatura Externa, Interna e do Alimento)
+   
+    #entrada = [ultima_temperatura_int, ultima_umidade_int, ultima_temperatura_ext, ultima_umidade_ext]
+    # temperatura_prevista = modelo_carregado.predict([entrada])[0]
+    # Formatando para exibir apenas dois dígitos após o ponto decimal
+    #temperatura_prevista_formatada = f'{temperatura_prevista:.2f}'
+    
+
+
     fig_temp = px.line(df, x="Tempo", y=["temperature_ext", "temperature_int", "temperature_alimento"], title="Temperatura Externa, Interna e do Alimento")
     fig_temp.update_xaxes(type="category")  # Usar categoria para o eixo x
     plot_temp_html = fig_temp.to_html(full_html=False)
 
-    # Criar gráfico usando Plotly (Umidade Interna e Umidade Externa)
+    
     fig_umidade = px.line(df, x="Tempo", y=["humidity_int", "humidity_ext"], title="Umidade Interna e Externa")
     fig_umidade.update_xaxes(type="category")  # Usar categoria para o eixo x
     plot_umidade_html = fig_umidade.to_html(full_html=False)
-
-    # Criar histogramas para os dados atuais
 
 
     return render_template("index.html", 
@@ -47,7 +58,9 @@ def index():
                            ultima_temperatura_int=ultima_temperatura_int, 
                            ultima_temperatura_ext=ultima_temperatura_ext,
                            ultima_umidade_int=ultima_umidade_int,
-                           ultima_umidade_ext=ultima_umidade_ext
+                           ultima_umidade_ext=ultima_umidade_ext,
+                           temperatura_atual_alimento = ultima_temperatura_alimento,
+                           #temperatura_prevista = temperatura_prevista_formatada
                            )
 
 if __name__ == '__main__':
